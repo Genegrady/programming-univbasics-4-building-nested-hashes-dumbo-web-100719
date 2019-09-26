f52e2523388ad627c6d8bcdb451b181b45b08639
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-26 16:26:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ end
 def monopoly_with_second_tier
  monopoly = {
     :railroads => {
-      :pieces => 4
+      :pieces => 4,
+      :rent_in_dollars => {
+        
+      }
       
     }
     
